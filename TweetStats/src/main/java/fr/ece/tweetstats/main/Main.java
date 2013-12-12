f2c9e9dcb44e5bbf95e016791ee2583991628d95
thibaultCha <thibaultcha@me.com>
2013-12-12 16:03:25 +0100

    Example of getByAdjectives()

--- TweetStats/src/main/java/fr/ece/tweetstats/main/Main.java
+++ TweetStats/src/main/java/fr/ece/tweetstats/main/Main.java
@@ -1,15 +1,26 @@
 package fr.ece.tweetstats.main;
 
+import java.util.Arrays;
+import java.util.List;
+
+import fr.ece.tweetstats.core.domain.Fetch;
+import fr.ece.tweetstats.core.domain.Tweet;
 import fr.ece.tweetstats.twitterapi.TwitterAPI;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		TwitterAPI.getByBrandAndAdjective("RATP", "grève");
 
+		List<String> adjectives = Arrays.asList("grève", "retard");
+		// Synchrone call to Twitter API
+		List<Fetch> fetchResults = TwitterAPI.getByBrandAndAdjectives("RATP", adjectives);
 		
-			TwitterAPI.getByBrandAndAdjective("RATP", "grève");
-
+		for (Fetch fetch : fetchResults) {
+			for (Tweet tweet : fetch.getResults()) {
+				System.out.println(tweet.getMessage());
+			}
+		}
 	}
 
 }
