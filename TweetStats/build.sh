0ca33907f074a8127e1a4e265ab2b77ce6824d85
thibaultCha <thibaultcha@me.com>
2013-12-17 11:23:40 +0100

    Compiling project in native distribution format

--- TweetStats/build.sh
+++ TweetStats/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #rm ./src/test/generated/java/fr/ece/tweetstats/core/serviceapi/*
-rm src/test/java/fr/ece/tweetstats/search/serviceapi/*.java
-rm src/test/java/fr/ece/tweetstats/core/serviceapi/*.java
-mvn package #-DskipTests=true
+rm ./src/test/java/fr/ece/tweetstats/search/serviceapi/*.java
+rm ./src/test/java/fr/ece/tweetstats/core/serviceapi/*.java
+
+# without javafx dependency
+#mvn package #-DskipTests=true
+
+#mvn jfx:jar
+mvn jfx:native
